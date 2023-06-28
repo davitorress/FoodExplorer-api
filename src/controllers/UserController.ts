4ee24812695dcd0d586e8bbcb3653e49111391f8
davitorress <dev.davitorres@gmail.com>
2023-06-28 14:51:08 -0300

    feat: users routes and methods

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -33,4 +33,57 @@ export class UserController {
 
 		return res.status(201).json();
 	}
+
+	async update(req: Request, res: Response) {
+		const bodySchema = z.object({
+			name: z.string(),
+			email: z.string().email({ message: "E-mail inválido!" }),
+			password: z
+				.string()
+				.min(6, { message: "A senha deve ter no mínimo 6 caracteres!" })
+				.max(20, { message: "A senha deve ter no máximo 20 caracteres!" })
+				.optional(),
+			old_password: z
+				.string()
+				.min(6, { message: "A senha deve ter no mínimo 6 caracteres!" })
+				.max(20, { message: "A senha deve ter no máximo 20 caracteres!" })
+				.optional(),
+		});
+
+		const { id } = req.params;
+		const result = bodySchema.safeParse(req.body);
+
+		if (!result.success) {
+			throw new AppError(result.error.issues[0].message);
+		}
+		const { name, email, password, old_password } = result.data;
+
+		const user = await knexCon("users").where({ id }).first();
+		if (!user) {
+			throw new AppError("Usuário não encontrado.");
+		}
+
+		const userWithUpdatedEmail = await knexCon("users").where({ email }).first();
+		if (userWithUpdatedEmail && userWithUpdatedEmail.id !== Number(id)) {
+			throw new AppError("Este e-mail já está em uso.");
+		}
+
+		user.name = name ?? user.name;
+		user.email = email ?? user.email;
+
+		if (password && !old_password) {
+			throw new AppError("A senha antiga deve ser informada.");
+		}
+
+		if (password && old_password) {
+			const checkOldPassword = await compare(old_password, user.password);
+			if (!checkOldPassword) {
+				throw new AppError("A senha antiga não confere.");
+			}
+			user.password = await hash(password, 8);
+		}
+
+		await knexCon("users").where({ id }).update(user);
+		return res.json(user);
+	}
 }
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -6,5 +6,6 @@ const usersRoutes = Router();
 const userController = new UserController();
 
 usersRoutes.post("/", userController.create);
+usersRoutes.put("/:id", userController.update);
 
 export { usersRoutes };
