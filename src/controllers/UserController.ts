0595bfb69afb2c7eafe2f894f88f45d60d4c1ce1
davitorress <dev.davitorres@gmail.com>
2023-06-28 14:26:00 -0300

    refactor: bodySchema on class method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,22 +1,21 @@
+import { z } from "zod";
 import { hash, compare } from "bcryptjs";
 import { Request, Response } from "express";
 
 import { knexCon } from "../database/knex";
 import { AppError } from "../utils/AppError";
-import { z } from "zod";
-
-const createUserBody = z.object({
-	name: z.string(),
-	email: z.string().email(),
-	password: z
-		.string()
-		.min(6, { message: "A senha deve ter no mínimo 6 caracteres!" })
-		.max(20, { message: "A senha deve ter no máximo 20 caracteres!" }),
-});
 
 export class UserController {
 	async create(req: Request, res: Response) {
-		const result = createUserBody.safeParse(req.body);
+		const bodySchema = z.object({
+			name: z.string(),
+			email: z.string().email({ message: "E-mail inválido!" }),
+			password: z
+				.string()
+				.min(6, { message: "A senha deve ter no mínimo 6 caracteres!" })
+				.max(20, { message: "A senha deve ter no máximo 20 caracteres!" }),
+		});
+		const result = bodySchema.safeParse(req.body);
 
 		if (!result.success) {
 			throw new AppError(result.error.issues[0].message);
