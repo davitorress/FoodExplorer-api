91bbf54f8a3642a689efcbf778d47733dad3a9d7
davitorress <dev.davitorres@gmail.com>
2023-06-28 14:50:27 -0300

    feat: unique email for users

--- src/database/knex/migrations/20230628145728_createUser.ts
+++ src/database/knex/migrations/20230628145728_createUser.ts
@@ -8,8 +8,8 @@ export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable("users", (table) => {
 		table.increments("id").primary();
 		table.string("name").notNullable();
-		table.string("email").notNullable();
 		table.string("password").notNullable();
+		table.string("email").unique().notNullable();
 
 		table.timestamp("created_at").defaultTo(knex.fn.now());
 		table.timestamp("updated_at").defaultTo(knex.fn.now());
