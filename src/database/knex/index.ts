99c5b6760192e569090f56334a9069feaf589d1a
davitorress <dev.davitorres@gmail.com>
2023-06-28 14:20:49 -0300

    refactor: rename kenxConnection variable

--- src/database/knex/index.ts
+++ src/database/knex/index.ts
@@ -1,4 +1,4 @@
 import { knex } from "knex";
 import knexConfig from "../../../knexfile";
 
-export const connection = knex(knexConfig.development);
+export const knexCon = knex(knexConfig.development);
